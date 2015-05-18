386739b751fe2290fd21eed010605b00fa40c78c
luiarthur <luiarthur@gmail.com>
2015-05-17 23:59:29 -0600

    yes

--- fireapp.js
+++ fireapp.js
@@ -1,6 +1,25 @@
-var ref = new Firebase("https://luifireapp.firebaseio.com/comments/fireapp");
 var myUserID = null;
+var ref = new Firebase("https://luifireapp.firebaseio.com/comments/");
 
-function onGoogleLogin() {
+function onLoginClick() {
   ref.authWithOAuthPopup("google",function(){});
+}
+
+ref.onAuth(function(authData) {
+  if (authData) {
+    myUserID = authData.google.id;
+    $("#loginDiv").text(authData.google.displayName);
+  }
+});
+
+function onCommentKeyDown(event) {
+  if (event.keyCode == 13) {
+    if(myUserID == null) {
+      alert("You must log in to leave a comment");
+    } else {
+      console.log(myUserID+" successfully commented: "+$("#newComment").val());
+      ref.push({userid: myUserID, body: $("#newComment").val()})
+    }
+    event.preventDefault(); // prevents default actions
+  }
 }
