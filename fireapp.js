7fea852dfedb2c9d5fda1640aa7fc3d3ff30eafa
luiarthur <luiarthur@gmail.com>
2015-05-17 18:59:24 -0600

    yes

--- fireapp.js
+++ fireapp.js
@@ -1,13 +1,6 @@
 var ref = new Firebase("https://luifireapp.firebaseio.com/comments/fireapp");
-// prefer pop-ups, so we don't navigate away from the page
-ref.authWithOAuthPopup("google", function(error, authData) {
-  if (error) {
-    if (error.code === "TRANSPORT_UNAVAILABLE") {
-      // fall-back to browser redirects, and pick up the session
-      // automatically when we come back to the origin page
-      ref.authWithOAuthRedirect("google", function(error) { /* ... */ });
-    }
-  } else if (authData) {
-    // user authenticated with Firebase
-  }
-});
+var myUserID = null;
+
+function onGoogleLogin() {
+  ref.authWithOAuthPopup("google",function(){});
+}
