ddfb786e92b1b5610c60962a4493965c08bd8d2e
luiarthur <luiarthur@gmail.com>
2015-05-19 01:25:05 -0600

    yes

--- fireapp.js
+++ fireapp.js
@@ -9,6 +9,9 @@ ref.onAuth(function(authData) {
   if (authData) {
     myUserID = authData.google.id;
     $("#loginDiv").text(authData.google.displayName);
+    var src = "https://www.googleapis.com/plusDomains/v1/people/"+authData.uid;
+    $("userPic").attr("src",src);
+    console.log(src);
   }
 });
 
@@ -18,8 +21,27 @@ function onCommentKeyDown(event) {
       alert("You must log in to leave a comment");
     } else {
       console.log(myUserID+" successfully commented: "+$("#newComment").val());
-      ref.push({userid: myUserID, body: $("#newComment").val()})
+      ref.push({userid: myUserID, body: $("#newComment").val()});
+      $("#newComment").val("");
     }
     event.preventDefault(); // prevents default actions
   }
 }
+
+//Create a query for only the last 4 comments
+var last4Comments = ref.limit(4);
+
+//Render Comments
+last4Comments.on('child_added', function (snapshot) {
+  var comment = snapshot.val();
+  var newDiv = $("<div/>").addClass("comment").attr("id",snapshot.name()).appendTo("#oldComments");
+  FB.api("/" + comment.userid, function(userdata) {
+    comment.name = userdata.name;
+    newDiv.html(Mustache.to_html($('#template').html(), comment));
+  });
+});
+
+//Remove deleted comments
+last4Comments.on("child_removed", function(snapshot) {
+  $("#" + snapshot.name()).remove();
+});
