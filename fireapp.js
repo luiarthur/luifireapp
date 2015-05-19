453707ee9080060aea209a12df702af384f40517
luiarthur <luiarthur@gmail.com>
2015-05-19 16:21:04 -0600

    yes

--- fireapp.js
+++ fireapp.js
@@ -1,17 +1,57 @@
 var myUserID = null;
-var ref = new Firebase("https://luifireapp.firebaseio.com/comments/");
+var myName = null;
+var myPicture = null;
+var slug = (window.location.href).replace(/\/|\.|:|\[|\]|\#|\$\-/g,"");
+var link = "https://luifireapp.firebaseio.com/comments";
+var ref = new Firebase(link);
+// Store: name, uid, body, picture
 
-function onLoginClick() {
-  ref.authWithOAuthPopup("google",function(){});
+//var temp = new Firebase("https://luifireapp.firebaseio.com/comments/test");
+//temp.remove();console.log("yes");
+
+
+function onLoginClick(provider) {
+  ref.authWithOAuthPopup(provider,function(){});
+}
+function onLogoutClick() {
+  ref.unauth();
 }
 
 ref.onAuth(function(authData) {
+//var login = "<a href='#' onclick='onLoginClick()' id='login'>login</a>";
+var login = "<img id='githubLogin' href='#' onclick='onLoginClick(\"github\")'   src='logos/github.png'>"+
+            "<img id='googleLogin' href='#' onclick='onLoginClick(\"google\")'   src='logos/google.png'>"+ 
+            "<img id='fbLogin'     href='#' onclick='onLoginClick(\"facebook\")' src='logos/fb.png'>";
+var logout = "<a href='#' onclick='onLogoutClick()' id='logout'>logout</a>";
+
   if (authData) {
-    myUserID = authData.google.id;
-    $("#loginDiv").text(authData.google.displayName);
-    var src = "https://www.googleapis.com/plusDomains/v1/people/"+authData.uid;
-    $("userPic").attr("src",src);
-    console.log(src);
+    switch(authData.provider) {
+      case "google": 
+        myUserID = authData.google.id; 
+        myName = authData.google.displayName; 
+        myPicture = authData.google.cachedUserProfile.picture;
+        break;
+      case "facebook":
+        myUserID = authData.facebook.id; 
+        myName = authData.facebook.displayName; 
+        myPicture = authData.facebook.cachedUserProfile.picture.data.url;
+        break;
+      case "github":
+        myUserID = authData.github.id; 
+        myName = authData.github.displayName; 
+        myPicture = authData.github.cachedUserProfile.avatar_url;
+        break;
+    }
+    $("#logIO").text("").append(logout);
+    $("#userPic").attr("src",myPicture);
+    $("#newComment").attr("placeholder",myName+"' s comment...");
+  } else {
+    $("#logIO").text("").append(login);
+    myName=null;
+    myUserID=null;
+    myPicture="http://sigaramae.org/img/team-placeholder-man.jpg";
+    $("#userPic").attr("src",myPicture);
+    $("#newComment").attr("placeholder","Please login to comment...");
   }
 });
 
@@ -20,9 +60,12 @@ function onCommentKeyDown(event) {
     if(myUserID == null) {
       alert("You must log in to leave a comment");
     } else {
-      console.log(myUserID+" successfully commented: "+$("#newComment").val());
-      ref.push({userid: myUserID, body: $("#newComment").val()});
-      $("#newComment").val("");
+      if (event.shiftKey) {
+        $("#newComment").val($("#newComment").val()+"\n");
+      } else {
+        ref.push({userid: myUserID, body: $("#newComment").val(), name: myName, picture:myPicture});
+        $("#newComment").val("");
+      }
     }
     event.preventDefault(); // prevents default actions
   }
@@ -35,13 +78,12 @@ var last4Comments = ref.limit(4);
 last4Comments.on('child_added', function (snapshot) {
   var comment = snapshot.val();
   var newDiv = $("<div/>").addClass("comment").attr("id",snapshot.name()).appendTo("#oldComments");
-  FB.api("/" + comment.userid, function(userdata) {
-    comment.name = userdata.name;
-    newDiv.html(Mustache.to_html($('#template').html(), comment));
-  });
+  newDiv.html(Mustache.to_html($('#template').html(), comment));
 });
 
 //Remove deleted comments
 last4Comments.on("child_removed", function(snapshot) {
   $("#" + snapshot.name()).remove();
 });
+
+$("#newComment").elastic();
